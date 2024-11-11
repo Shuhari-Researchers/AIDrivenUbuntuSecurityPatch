71fe6179d6797becc4e16a444532c1439f9d2c5a
berrysu42 <berikaguvenc@gmail.com>
2024-11-11 21:23:42 +0300

    2 farklı dosya da aynı amaçla oluşturulmuştur deneyebilirsiniz

---
+++ Automation/Service/ShuhariService.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# This is a basic script. It shows the service functions.
+
+echo "Hello Shuhari Researchers Team! This script is managed by the system key."
+echo "Current date and time: $(date)"
