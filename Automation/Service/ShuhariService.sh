71af2f923237be9a9468b27b8c450efbe8e8bb03
berrysu42 <berikaguvenc@gmail.com>
2024-11-27 14:54:34 +0300

    /home/berika degiskenini kullanarak güncelleme yaptım.

--- Automation/Service/ShuhariService.sh
+++ Automation/Service/ShuhariService.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-# This is a basic script. It shows the service functions.
+# Bu script, Shuhari servisini yönetir.
 
 echo "Hello Shuhari Researchers Team! This script is managed by the system key."
 echo "Current date and time: $(date)"
+
+# Örneğin, Automation dizininde bir işlem yapmak
+echo "Performing task in Automation directory..."
+ls $HOME/Automation
+
